bec18e4683898881746bf56c7754765d2ca5ef18
rakeshls <73830663+rakeshls@users.noreply.github.com>
2023-04-25 19:43:57 +0530

    Add files via upload

---
+++ client.py
@@ -0,0 +1,27 @@
+import socket
+from threading import Thread
+from tkinter import *
+from tkinter import ttk
+
+
+PORT  = 8080
+IP_ADDRESS = '127.0.0.1'
+SERVER = None
+BUFFER_SIZE = 4096
+
+
+
+
+
+
+def setup():
+    global SERVER
+    global PORT
+    global IP_ADDRESS
+
+    SERVER = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    SERVER.connect((IP_ADDRESS, PORT))
+
+
+setup()
+
